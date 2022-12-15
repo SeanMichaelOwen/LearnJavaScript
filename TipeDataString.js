6016f78a364f5195ec1db629ae1fc525425e2b41
Sean Michael <120260830+SeanMichaelOwen@users.noreply.github.com>
2022-12-15 22:39:13 +0800

    04 Tipe Data String

---
+++ TipeDataString.js
@@ -0,0 +1,6 @@
+console.log('Hello Coders')
+console.log("Hello, My Name Is Sean Michael")
+console.info("Age 17")
+console.info("Skills {Cyber Security, Web Developer}")
+
+console.log("Hello " + " World")
