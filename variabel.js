621ca9f88e0af278c65657246cf0e3b42ad79d67
Sean Michael <120260830+SeanMichaelOwen@users.noreply.github.com>
2022-12-15 22:57:03 +0800

    05 Variabel

---
+++ variabel.js
@@ -0,0 +1,10 @@
+var fullName;
+fullName = 'Sean Michael Owen'
+console.log(fullName)
+
+let Age;
+Age = 17
+console.info(Age)
+
+const Study = "{JavaScript, Java, Python, Golang, Matlab}"
+console.log(Study)
