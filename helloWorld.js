c471240050b29b53b3f13a2795f5f9d566f02857
Sean Michael <120260830+SeanMichaelOwen@users.noreply.github.com>
2022-12-15 22:31:55 +0800

    02 Komentar Di Java Script

--- helloWorld.js
+++ helloWorld.js
@@ -1,4 +1,4 @@
-console.log("Hello World & Hello Komentar")
+console.log("Hello World & Hello Komentar!")
 // Ini Adalah Komentar satu baris di java script
 /* Ini adalah komentar beribu baris
  Ini lihat yaa
