10af40927fbaaec3071437349bdae2f67cc7ab10
Sean Michael <120260830+SeanMichaelOwen@users.noreply.github.com>
2022-12-16 17:40:37 +0800

    14 If Expression

---
+++ IfExpression.js
@@ -0,0 +1,11 @@
+const examValue = 0
+
+if(examValue === 100){
+    console.log("Great Job 100%")
+}else if(examValue >= 80){
+    console.log(`Good Job ${examValue}%`)
+}else if(examValue){
+    console.log(`Tray again ${examValue}%`)
+}else{
+    console.log("Your Lose 0%")
+}
