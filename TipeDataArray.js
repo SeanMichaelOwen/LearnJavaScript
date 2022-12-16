f53dadb917575c79e9ac62c26582c75b43035511
Sean Michael <120260830+SeanMichaelOwen@users.noreply.github.com>
2022-12-16 16:42:59 +0800

    12 Tipe Data Array

---
+++ TipeDataArray.js
@@ -0,0 +1,11 @@
+let arrayKosong = [] //Array Kosong
+let arrayBox = ["Box0", "Box1", "Box2"] //Mendeklarasi Array
+console.log(arrayBox[1])
+
+//Menambah data ke array
+arrayKosong.push("Array In Java Script")
+arrayKosong.push("Array1", "Array2", 10)
+console.table(arrayKosong)
+delete arrayKosong[3]
+console.info(arrayKosong[3])
+console.table(arrayKosong)
