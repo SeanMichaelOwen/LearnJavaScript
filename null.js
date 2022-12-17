13277902afafe797db66ded7f9bb30c48ea6fe1d
Sean Michael <120260830+SeanMichaelOwen@users.noreply.github.com>
2022-12-17 10:26:31 +0800

    17 Null
    Signed-off-by: Sean Michael <120260830+SeanMichaelOwen@users.noreply.github.com>

---
+++ null.js
@@ -0,0 +1,9 @@
+let fristName = null;
+
+if(fristName === undefined){
+    console.info("Hallo Undefined")
+}else if(fristName === null){
+    console.info("Hallo Null")
+}else{
+    console.info(`Hello ${fristName}`)
+}
